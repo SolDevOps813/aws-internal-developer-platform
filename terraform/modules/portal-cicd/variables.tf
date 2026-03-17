2a66eaf355dee9724ee472eb3ae25a2e7d1a8c6d
SolDevOps813 <michael.williams@solstice.tech>
2026-03-17 12:33:52 -0400

    Create variables.tf

---
+++ terraform/modules/portal-cicd/variables.tf
@@ -0,0 +1,15 @@
+variable "github_owner" {
+  type = string
+}
+
+variable "github_repo" {
+  type = string
+}
+
+variable "github_branch" {
+  type = string
+}
+
+variable "cloudfront_distribution_id" {
+  type = string
+}
